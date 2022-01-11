d651e5d0e81714f311623c637b8ff40de45ca26f
mospito <jaturonmoonjan@gmail.com>
2022-01-12 06:33:40 +0700

    commit nav gameplay

--- components/navbar.js
+++ components/navbar.js
@@ -39,6 +39,11 @@ const Navbar = () => (
                         ITEMS
                     </a>
                 </Link>
+                <Link href='/gameplay'>
+                    <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 mr-8 rounded text-white font-bold items-center justify-center hover:bg-blue hover:text-white'>
+                        GAME PLAY
+                    </a>
+                </Link>
                 {/* <Link href='/#'>
                     <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 mr-8 rounded text-white font-bold items-center justify-center hover:bg-blue hover:text-white'>
                         LOG OUT
