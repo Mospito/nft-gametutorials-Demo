066b116c3515fb67b4f4b9d778cd6052def81144
sirinyapat <sirinyapat.b@gmail.com>
2022-04-19 03:19:00 +0700

    Edit CSS all_exam

--- pages/Admin/all_exam.js
+++ pages/Admin/all_exam.js
@@ -7,6 +7,8 @@ import axios from 'axios'
 import 'antd/dist/antd.css';
 import styled from 'styled-components';
 import { Form, Input, Button, Radio } from 'antd';
+import conyPhoto from '../../photos/cony.png'
+import Image from 'next/image'
 
 const URL = `http://178.128.90.50:4444/quizs`
 
@@ -44,21 +46,44 @@ export default function AllExam() {
     const render = () => {
 
         return (data.map((item, index) => (
-            <div class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-               
-                    <text>{index + 1}</text>
-                    <text>{item.question}</text>
-                    <text>{item.choice1}</text>
-                    <text> {item.choice2}</text>
-                    <text> {item.choice3} </text>
-                    <text>{item.choice4} </text>
-                    <text>{item.ans}</text>
-                       
+            <div className="flex flex-col justify-start items-start w-1/3 h-3/4 p-4 bg-white rounded-lg text-left border-2 border-red-800 shadow-md">
+                <div className='flex flex-col w-full h-4/5 text-justify overflow-y-auto'>
+                    <div className='flex flex-row font-semibold text-justify w-11/12'>
+                        <text>{index + 1}.</text>
+                        <text className='pl-1'>{item.question}</text>
+                    </div>
+                    <div className='flex flex-col text-justify w-11/12 mt-2'>
+                        <div>
+                            <text className='font-semibold'>A.</text>
+                            <text className='pl-1'>{item.choice1}</text>
+                        </div>
+                        <div>
+                            <text className='font-semibold'>B.</text>
+                            <text className='pl-1'>{item.choice2}</text>
+                        </div>
+                        <div>
+                            <text className='font-semibold'>C.</text>
+                            <text className='pl-1'>{item.choice3}</text>
+                        </div>
+                        <div>
+                            <text className='font-semibold'>D.</text>
+                            <text className='pl-1'>{item.choice4}</text>
+                        </div>
+                    </div>
 
-                    <button>Edit</button>
-                    <button>Delete</button>
-                    
-                
+
+                    <div className='flex flex-row mt-2 text-justify font-semibold w-11/12'>
+                        <text className='text-red-700'>Answer:</text>
+                        <text className='pl-1'>{item.ans}</text>
+                    </div>
+                </div>
+
+
+                <div className='flex flex-row justify-center items-center gap-8 w-full pt-6 h-1/5'>
+                    <button className='flex justify-center items-center border-2 border-green-600 h-9 w-20 rounded-lg bg-babygreen hover:bg-whiteblue font-semibold shadow-lg'>Edit</button>
+                    <button className='flex justify-center items-center border-2 border-red-500 h-9 w-20 rounded-lg bg-red2 hover:bg-red3 font-semibold shadow-lg'>Delete</button>
+                </div>
+
             </div>
 
         )))
@@ -66,12 +91,19 @@ export default function AllExam() {
 
 
     return (
+        <div className='flex flex-col fixed justify-start items-center h-screen w-screen mt-32'>
+            <div className='flex flex-row justify-center w-full items-center -mt-24'>
+                <div className='flex flex-col justify-center items-center text-4xl w-1/5 h-14 mr-4 p-4 font-bold uppercase tracking-wider shadow-xl bg-sunglow rounded-lg pointer-events-none'>
+                    <a className='flex justify-center text-2xl font-bold text-black animate-pulse'>Edit Exam</a>
+                </div>
+                <div className='-ml-16 -mt-6'>
+                    <Image src={conyPhoto} width="100%" height="100%" className='' />
+                </div>
+            </div>
+            <div className="flex flex-wrap gap-16 justify-center items-center w-4/5 h-2/3 mt-4 p-4 bg-redcony rounded-2xl shadow-md overflow-y-auto">
+                {render()}
+            </div>
 
-
-        <div className='flex flex-start items-start drop-shadow-2xl pt-5' >
-
-
-            {render()}
         </div>
 
 
